f0d43117ea3ff1f29d30ffb7f88f60864614a7a7
Sam Dubois <samdubois18@icloud.com>
2018-01-05 15:05:31 -0500

    Initial Commit

---
+++ dev-profile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by Sam DuBois on 1/5/18.
+//  Copyright © 2018 Sam DuBois. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
